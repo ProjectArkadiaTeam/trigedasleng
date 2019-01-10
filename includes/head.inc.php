16356a964e30ac4bdef0bec06b4992827a8882e4
Stephano <stephano@projectarkadia.com>
2019-01-11 00:31:15 +0100

    Added favicon

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -5,6 +5,7 @@
     <meta name="description" content="Trigedasleng is a conlang created bt David J. Peterson for The TV-Show The100">
     <meta name="keywords" content="The100,Dictionary,Unoffical,Trigedasleng">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
     <link rel="stylesheet" type="text/css" href="./css/main.css?v=13512.10" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="./js/main.js"></script>

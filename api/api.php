3ee5c368e7362557933f486038f19b5c7e0363be
Stefan Rasmussen <stefan@stemaki.dk>
2019-03-26 16:33:14 +0100

    Added getDictionary action to API

--- api/api.php
+++ api/api.php
@@ -12,19 +12,25 @@
     require_once("../includes/common.inc.php");
 
     // Call the chosen action
-    if (isset($_GET['action']) && !empty($_GET['action']) && isset($_GET['query'])){
+    if (isset($_GET['action']) && !empty($_GET['action'])){
         //Check if action exists
         if(function_exists($_GET['action'])) {
             //Call action
-            $_GET['action']($db, $_GET['query']);
+            $_GET['action']($db);
 
         } else {
-            //TODO: Error reporting
+            echo 'Action is INVALID';
+            exit();
         }
     }
 
-    function search($db, $query){
-        $query = mysqli_real_escape_string($db, $query);
+    function search($db){
+        if(!isset($_GET['query']) || empty($_GET['query'])) {
+            echo "A search query is required!";
+            exit();
+        }
+
+        $query = mysqli_real_escape_string($db, $_GET['query']);
         $words_query = "SELECT * FROM `dict_words` WHERE (`word` LIKE '%".$query."%') OR (`translation` LIKE '%".$query."%')";
         $translation_query = "SELECT * FROM `dict_translations` WHERE (`trigedasleng` LIKE '%".$query."%') OR (`translation` LIKE '%".$query."%')";
         $words_result = $db->query($words_query);
@@ -38,6 +44,33 @@
 
         while($translation = mysqli_fetch_array($translation_result, MYSQLI_ASSOC)){
             $data['translations'][] = $translation;
+        }
+
+        echo json_encode($data, JSON_PRETTY_PRINT);
+    }
+
+    function getDictionary($db) {
+        if(!isset($_GET['filter']) || empty($_GET['filter'])) {
+            echo "Dictionary filter is required!";
+            exit();
+        }
+
+        $filter = mysqli_real_escape_string($db, $_GET['filter']);
+        $filterTerms = explode(' ', $filter);
+        $filterTermBits = array();
+        foreach ($filterTerms as $term) {
+            $term = trim($term);
+            if (!empty($term)) {
+                $filterTermBits[] = "`filter` RLIKE '[[:<:]]".$term."[[:>:]]'";
+            }
+        }
+
+        $sql = "SELECT * FROM `dict_words` WHERE ".implode(' AND ', $filterTermBits)." ";
+        $result = $db->query($sql);
+
+        $data = array();
+        while ($word = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $data[] = $word;
         }
 
         echo json_encode($data, JSON_PRETTY_PRINT);

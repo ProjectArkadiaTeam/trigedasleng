9989d6eddd56b3b8a4a6b7e66cbf6eefe0907b05
PotatoPlank <jduell1337@gmail.com>
2020-05-18 16:25:59 -0400

    Added a default 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default.app')
+
+@section('title', "Page Not Found")
+
+@section('content')
+    <h1 style="display:flex;justify-content:center;">Page not found!</h1>
+    <h3 style="display:flex;justify-content:center;">The page you requested does not exist.</h3>
+@endsection

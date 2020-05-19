d0e9344b1ad95b0f859d43ba2cc489e830b48575
PotatoPlank <jduell1337@gmail.com>
2020-05-19 02:05:17 -0400

    Database migrations & Cleanup Users, Group, and OAuth/Passport table migrations. Passport configuration. Removed default User controller

---
+++ database/migrations/2020_05_18_221416_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->uuid('group_id');
+            $table->foreign('group_id')->references('id')->on('groups');
+            $table->timestamp('verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
